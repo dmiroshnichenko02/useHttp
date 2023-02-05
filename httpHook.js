252f158a92dbbd02d3c7ee7d73fb3ee722c9921f
Danil <danil.miroshnichenko2022@gmail.com>
2023-02-05 09:58:00 +0200

    Fetch Hook

---
+++ httpHook.js
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react";
+
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+
+        setLoading(loading => true)
+
+        try {
+            const response = await fetch(url,{method, body, headers});
+
+            if (!response.ok) {
+                throw new Error(`Could not fetch ${url}, status ${response.status}`)
+            }
+
+            const data = await response.json();
+
+            setLoading(loading => false)
+
+            return data
+        } catch(e) {
+            setLoading(loading => false)
+
+            setError(e.message);
+            throw e;
+        }
+    }, []);
+
+    const clearError = useCallback(() => setError(null), [])
+
+    return {request, loading, error, clearError}
+}
+export default useHttp;
